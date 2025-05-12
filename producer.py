b7925372bff2ac0583daa5d66cf563c08ae43b02
Gera <gera51666@gmail.com>
2025-05-12 18:55:52 +0300

    доработать все

--- producer.py
+++ producer.py
@@ -1,12 +1,23 @@
+import os
+import json
+
 import telebot 
 import sqlite3 
+import toml
 from telebot import types
 from io import StringIO 
-import os
 from dotenv import load_dotenv
 from pathlib import Path
-env_path = Path(__file__).parent / 'api.env'
 
+from database import engine, start_table
+from schemasdb import Base
+
+Base.metadata.create_all(bind=engine)
+
+with open('data.json', 'r', encoding='utf-8') as f:
+    data = json.load(f)
+
+env_path = Path(__file__).parent / 'file.env'
 if env_path.exists():
     print(f"Файл .env найден по пути: {env_path}")
 else:
@@ -15,8 +26,7 @@ else:
 load_dotenv(dotenv_path=env_path)
 BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
 bot = telebot.TeleBot(BOT_TOKEN)
-
-
+ADMIN = os.getenv('ADMIN')
 
 quanity = 0 #ИНДЕКС СПИСКА ПО КЛЮЧУ В СЛОВАРЕ ВЫБОРА ТОВАРА
 choice_tovar = None # ЗНАЧЕНИЕ ИЗ СЛОВАРЯ ВЫБОРА ТОВАРА
@@ -33,69 +43,8 @@ zakup = 0 # СУММИРУЕТ price_for_admin ДЛЯ ИТОГОВОЙ ЦЕНЫ 
 zakup_rus = 0 # СУММИРУЕТ price_for_admin ДЛЯ ИТОГОВОЙ ЦЕНЫ РУССКОГО ПРАЙСА(АПОКВЕЛ И НЕПТРА)
 last_id = 0 # ПРИ УДАЛЕНИИ ПОЛЬЗОВАТЕЛЕМ ДАННЫХ
 
-name_of_vakcina = ['1. Нобивак', '2. Эурикан', '3. Пуревакс', '4. Биокан', '5. Биофел']
-choice_name_vakcina = {
-1: ['', '1. Нобивак Dhppi', '2. Нобивак RL', '3. Нобивак Lepto', '4. Нобивак R 1/1', '5. Нобивак R 1/10', '6. Нобивак Tricat', '7. Нобивак KC', '8. Нобивак Puppy'],
-2: ['', '1. Эурикан Dhppi-L', '2. Эурикан Rabisin'],
-3: ['', '1. Пуревакс RcpCh', '2. Пуревакс Rcp'],
-4: ['', '1. Биокан+R', '2. Биокан'],
-5: ['', '1. Биофел', '2. Биофел+R'],
-}
-
-name_of_preparati = ['1. Бравекто', '2. Симпарика', '3. Нексгард', '4. Коронакэт', '5. Апоквел', '6. Кортавет', '7. Нептра', '8. Отоксолан']
-choice_of_preparati = {
-    1: ['', '1. Бравекто 2-5', '2. Бравекто 5-10', '3. Бравекто 10-20', '4. Бравекто 20-40', '5. Бравето 40-56'], 
-    2: ['', '1. Симпарика 1-2.5', '2. Симпарика 2.5-5', '3. Симпарика 5-10', '4. Симпарика 10-20', '5. Симпарика 20-40', '6. Симпарика 40-60'],
-    3: ['', '1. Нексгард 2-4', '2. Нексгард 4-10', '3. Нексгард 10-25', '4. Нексгард 25-50'],
-    4: ['', '1. Коронакэт 10мг', '2. Коронакэт 30мг'],
-    5: ['', '1. Апоквел 5.4мг', '2. Апоквел 16мг'],
-    6: ['', '1. Кортавет'],
-    7: ['', '1. Нептра'],
-    8: ['', '1. Отоксолан']
-}
-
-name_of_feed = ['1. Royal Canin', '2. Monge']
-choice_of_feed = {
-    1: ['', '1. RC Urinary Cat 85g', '2. RC Renal Cat 85g', '3. RC Gastro Cat 85g',
-            '4. RC Gastro MD Cat 85g', '5. RC Recovery ', '6. RC Baby Cat 195g', '7. RC Baby Cat Milk',
-            '8. RC Gastro Puppy 195g', '9. RC Gastro Dog 400g', '10.RC Gastro MD Dog 400g', '11.RC Baby Dog 195g', '12.RC Baby Dog Milk'],
-    2: ['', '1. MG Hypo Duck Dog 400g', '2. MG Gastro Dog 400g', '3. MG Urinary Cat 100g', '4. MG Sterile Cat 1,5kg',
-            '5. MG Gastro Dog 2kg', '6. MG Gastro Puppy 1,5kg', '7. MG Gastro Cat 1,5kg']
-}
 
 
-price_for_product = {
-    'Бравекто 2-5': 3500, 'Бравекто 5-10': 3600, 'Бравекто 10-20': 4000, 'Бравекто 20-40': 4300, 'Бравето 40-56': 4600,
-    'Симпарика 1-2.5': 3100, 'Симпарика 2.5-5': 3450, 'Симпарика 5-10': 3600, 'Симпарика 10-20': 3950, 'Симпарика 20-40': 4300,
-    'Симпарика 40-60': 4550, 'Нексгард 2-4': 2600, 'Нексгард 4-10': 2900, 'Нексгард 10-25': 3200, 'Нексгард 25-50': 3500, 'Нобивак Dhppi': 1250,
-    'Нобивак RL': 950, 'Нобивак Lepto': 550, 'Нобивак R 1/1': 650, 'Нобивак R 1/10': 4300, 'Нобивак Tricat': 1800, 'Нобивак KC': 460,
-    'Нобивак Puppy': 900, 'Эурикан Dhppi-L': 1300, 'Пуревакс RcpCh': 1800, 'Пуревакс Rcp': 1400, 'Эурикан Rabisin': 700, 'Биокан+R': 1200,
-    'Биокан': 1100, 'Биофел': 1200, 'Биофел+R': 1350, 'Коронакэт 10мг': 2700, 'Коронакэт 30мг': 4600, 'Апоквел 5.4мг': 16000,
-    'Апоквел 16мг': 22000, 'Кортавет': 1450, 'Нептра': 1700, 'Отоксолан': 1300, 'RC Urinary Cat 85g': 140, 'RC Renal Cat 85g': 140,
-    'RC Gastro Cat 85g': 140, 'RC Gastro MD Cat 85g': 140, 'RC Recovery ': 210, 'RC Baby Cat 195g': 180, 'RC Baby Cat Milk': 2100,
-    'RC Gastro Puppy 195g': 210, 'RC Gastro Dog 400g': 290, 'RC Gastro MD Dog 400g': 290, 'RC Baby Dog 195g': 180, 'RC Baby Dog Milk': 2100,
-    'MG Hypo Duck Dog 400g': 280, 'MG Gastro Dog 400g': 300, 'MG Urinary Cat 100g': 140, 'MG Sterile Cat 1,5kg': 1600, 'MG Gastro Dog 2kg': 2600,
-    'MG Gastro Puppy 1,5kg': 2100, 'MG Gastro Cat 1,5kg': 2350
-}
-
-price_for_admin = {
-    'Бравекто 2-5': 0, 'Бравекто 5-10': 0, 'Бравекто 10-20': 0, 'Бравекто 20-40': 0, 'Бравето 40-56': 0,
-    'Симпарика 1-2.5': 0, 'Симпарика 2.5-5': 0, 'Симпарика 5-10': 0, 'Симпарика 10-20': 0, 'Симпарика 20-40': 0,
-    'Симпарика 40-60': 0, 'Нексгард 2-4': 0, 'Нексгард 4-10': 0, 'Нексгард 10-25': 0, 'Нексгард 25-50': 0, 'Нобивак Dhppi': 0,
-    'Нобивак RL': 0, 'Нобивак Lepto': 0, 'Нобивак R 1/1': 0, 'Нобивак R 1/10': 0, 'Нобивак Tricat': 0, 'Нобивак KC': 0, 'Нобивак Puppy': 0,
-    'Эурикан Dhppi-L': 0, 'Пуревакс RcpCh': 0, 'Пуревакс Rcp': 0, 'Эурикан Rabisin': 0, 'Биокан+R': 0, 'Биокан': 0, 'Биофел': 0, 'Биофел+R': 0,
-    'Коронакэт 10мг': 0, 'Коронакэт 30мг': 0, 'Апоквел 5.4мг': 0, 'Апоквел 16мг': 0, 'Кортавет': 0, 'Нептра': 0, 'Отоксолан': 0, 'RC Urinary Cat 85g': 0,
-    'RC Renal Cat 85g': 0, 'RC Gastro Cat 85g': 0, 'RC Gastro MD Cat 85g': 0, 'RC Recovery ': 0, 'RC Baby Cat 195g': 0, 
-    'RC Baby Cat Milk': 0, 'RC Gastro Puppy 195g': 0, 'RC Gastro Dog 400g': 0, 'RC Gastro MD Dog 400g': 0, 'RC Baby Dog 195g': 0, 'RC Baby Dog Milk': 0,
-    'MG Hypo Duck Dog 400g': 0, 'MG Gastro Dog 400g': 0, 'MG Urinary Cat 100g': 0, 'MG Sterile Cat 1,5kg': 0, 'MG Gastro Dog 2kg': 0, 'MG Gastro Puppy 1,5kg': 0,
-     'MG Gastro Cat 1,5kg': 0}
-
-connection = sqlite3.connect('user_staff1.sql')
-c = connection.cursor()
-c.execute("CREATE TABLE IF NOT EXISTS user_all1(id INTEGER PRIMARY KEY AUTOINCREMENT, name_of_organization TEXT, name_of_product TEXT, price INTEGER, result_calculation_end INTEGER, admin INTEGER, admin_rus INTEGER, profit INTEGER)")
-connection.commit()
-c.close()
-connection.close()
 
 def get_main_menu_markup():
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
@@ -192,7 +141,7 @@ def backhome(call):
             )
         except:
             pass
-        msg = bot.send_message(call.message.chat.id, f'Введите номер вакцины: \n{'\n'.join(name_of_vakcina)}')
+        msg = bot.send_message(call.message.chat.id, f'Введите номер вакцины: \n{'\n'.join(data['vaccines']['name'])}')
         bot.register_next_step_handler(msg, lambda m: choice_buy(m, category='vakcina'))
     elif call.data == 'preparati':
         try:
@@ -203,7 +152,7 @@ def backhome(call):
             )
         except:
             pass
-        msg = bot.send_message(call.message.chat.id, f'Введите номер препарата: \n{'\n'.join(name_of_preparati)}')
+        msg = bot.send_message(call.message.chat.id, f'Введите номер препарата: \n{'\n'.join(data['preparation']['name'])}')
         bot.register_next_step_handler(msg, lambda m: choice_buy(m, category='preparati'))
     elif call.data == 'korma':
         try:
@@ -214,7 +163,7 @@ def backhome(call):
             )
         except:
             pass
-        msg = bot.send_message(call.message.chat.id, f'Введите номер корма: \n{'\n'.join(name_of_feed)}')
+        msg = bot.send_message(call.message.chat.id, f'Введите номер корма: \n{'\n'.join(data['feed']['name'])}')
         bot.register_next_step_handler(msg, lambda m: choice_buy(m, category='feed'))
     elif call.data == 'end':
         bot.send_message(call.message.chat.id, f"Спасибо за заказ, если хотите начать заново - напишите команду {'/start'}")
@@ -241,24 +190,22 @@ def choice_buy(message, category):
         number_of_list = int(message.text)
 
         if category == 'preparati':
-            if number_of_list not in choice_of_preparati or number_of_list < 1:
+            if number_of_list not in data['preparation']['name'] or number_of_list < 1:
                 raise IndexError("Неверный номер категории")
-            msg = bot.send_message(message.chat.id, f'введите номер препарата: \n{'\n'.join(choice_of_preparati[number_of_list])}' )
-            bot.register_next_step_handler(msg, lambda m: quanity_choice_buy(m, category='preparati'), )
+            msg = bot.send_message(message.chat.id, f'введите номер препарата: \n{'\n'.join(data['preparation']['name'][number_of_list])}' )
+            bot.register_next_step_handler(msg, lambda m: quanity_choice_buy(m, category='preparati'))
 
         if category == 'vakcina':
-            if number_of_list not in choice_name_vakcina  or number_of_list < 1:
+            if number_of_list not in data['vaccines']['name']  or number_of_list < 1:
                 raise IndexError("Неверный номер категории")
-            msg = bot.send_message(message.chat.id, f'введите номер вакцины: \n{'\n'.join(choice_name_vakcina[number_of_list])}' )
+            msg = bot.send_message(message.chat.id, f'введите номер вакцины: \n{'\n'.join(data['vaccines']['name'][number_of_list])}' )
             bot.register_next_step_handler(msg, lambda m: quanity_choice_buy(m, category='vakcina'))
 
         if category == 'feed':
-            if number_of_list not in choice_of_feed  or number_of_list < 1:
+            if number_of_list not in data['feed']['name']  or number_of_list < 1:
                 raise IndexError("Неверный номер категории")
-            msg = bot.send_message(message.chat.id, f'введите номер корма: \n{'\n'.join(choice_of_feed[number_of_list])}' )
+            msg = bot.send_message(message.chat.id, f'введите номер корма: \n{'\n'.join(data['feed']['name'][number_of_list])}' )
             bot.register_next_step_handler(msg, lambda m: quanity_choice_buy(m, category='feed'))
-
-
     except ValueError as e:
         msg = bot.send_message(
             message.chat.id,
@@ -292,16 +239,16 @@ def quanity_choice_buy(message, category):
             bot.register_next_step_handler(msg, quanity_goods)
             
         if category == 'vakcina':
-            if choice_name_vakcina[number_of_list][quanity] not in choice_name_vakcina[number_of_list] or quanity < 1:
+            if data['vaccines']['name'][number_of_list][quanity] not in data['vaccines']['name'][number_of_list] or quanity < 1:
                 raise IndexError('Неверный номер категории')
-            choice_tovar = choice_name_vakcina[number_of_list][quanity]
+            choice_tovar = data['vaccines']['name'][number_of_list][quanity]
             msg = bot.send_message(message.chat.id, f'Введите количество {choice_tovar[2:]}' )
             bot.register_next_step_handler(msg, quanity_goods)
 
         if category == 'feed':
-            if choice_of_feed[number_of_list][quanity] not in choice_of_feed[number_of_list] or quanity < 1:
+            if data['feed']['name'][number_of_list][quanity] not in data['feed']['name'][number_of_list] or quanity < 1:
                 raise IndexError('Неверный номер категории')
-            choice_tovar = choice_of_feed[number_of_list][quanity]
+            choice_tovar = data['feed']['name'][number_of_list][quanity]
             msg = bot.send_message(message.chat.id, f'Введите количество {choice_tovar[2:]}' )
             bot.register_next_step_handler(msg, quanity_goods)
 
@@ -342,7 +289,7 @@ def quanity_goods(message):
     else:
         zakup_rus += round((price_for_admin[choice_tovar[3:]]  * quanity_of_goods), 2)
     
-
+price_for_admin
     calculation_price_oneproduct = price_for_product[choice_tovar[3:]] * quanity_of_goods
     for_user = " Количество: " +  str(quanity_of_goods) + ' , ' + " Цена: " +  str(calculation_price_oneproduct)
     result_calculation += calculation_price_oneproduct
